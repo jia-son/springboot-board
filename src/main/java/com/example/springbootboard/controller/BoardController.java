2e7da15505765140a5d9f3a615f9bbac0727b157
jia-son <116625758+jia-son@users.noreply.github.com>
2023-05-25 15:52:11 +0900

    게시글 삭제

--- src/main/java/com/example/springbootboard/controller/BoardController.java
+++ src/main/java/com/example/springbootboard/controller/BoardController.java
@@ -69,4 +69,10 @@ public class BoardController {
         model.addAttribute("board", board);
         return "detail";
     }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
 }
